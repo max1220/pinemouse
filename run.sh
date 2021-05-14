f3741d6a568900bcdd05b7fb4a45c046e8ac7d78
Max <localhost>
2021-05-14 19:30:14 +0000

    Use static device names

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
-TOUCHSCREEN="/dev/input/event2"
-VOLUMEKEYS="/dev/input/event1"
+TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
+VOLUMEKEYS="/dev/input/by-path/platform-1c21800.lradc-event"
 
 while true; do
 	./detect_pattern.lua "${VOLUMEKEYS}" "+-+-+-"
