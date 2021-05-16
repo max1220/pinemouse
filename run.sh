6ab87f607dcac2d63ac88e98dea6bc30c09dc34d
Max <max@debian-fx>
2021-05-16 13:09:52 +0200

    Enable auto-rotation; Make run script independent of CWD

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+cd "$(dirname "${0}")"
 
 TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
 VOLUMEKEYS="/dev/input/by-path/platform-1c21800.lradc-event"
 
 while true; do
 	./detect_pattern.lua "${VOLUMEKEYS}" "+-+-+-"
-	./touchpad.lua "${TOUCHSCREEN}"
+	./touchpad.lua "${TOUCHSCREEN}" "$(./get_orientation.sh)"
 done
