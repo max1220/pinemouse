0fa92c0a61a8935ccb9c43a82b844410a72fae86
Max <max@debian-fx>
2021-05-26 16:21:04 +0200

    Updated launch script

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
 cd "$(dirname "${0}")"
 
 TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
 VOLUMEKEYS="/dev/input/by-path/platform-1c21800.lradc-event"
+VIBR="/dev/input/by-path/platform-vibrator-event"
 
 while true; do
 	./detect_pattern.lua "${VOLUMEKEYS}" "+-+-+-"
-	./touchpad.lua "${TOUCHSCREEN}" "$(./get_orientation.sh)"
+	./touchpad.lua "${TOUCHSCREEN}" "$(./get_orientation.sh)" "${VIBR}"
+	sleep 1
 done
