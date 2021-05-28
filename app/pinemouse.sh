c0ed7e4bc8195dd3b679387b15c8f4cfa49c5e3a
Max <max@debian-fx>
2021-05-28 14:43:50 +0200

    Setting up packaging for redistribution(WIP)

---
+++ app/pinemouse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "${0}")"
+
+TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
+VOLUMEKEYS="/dev/input/by-path/platform-1c21800.lradc-event"
+VIBR="/dev/input/by-path/platform-vibrator-event"
+
+while true; do
+	detect_pattern "${VOLUMEKEYS}" "+-+-+-"
+	touchpad "${TOUCHSCREEN}" "$(get_orientation.sh)" "${VIBR}"
+	sleep 1
+done
