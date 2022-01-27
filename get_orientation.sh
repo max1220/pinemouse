4c91f04535653e3c1e8c776aab94b5625e5e7349
Max <max@max1220.de>
2022-01-28 00:23:54 +0100

    Better orientation detection

--- get_orientation.sh
+++ get_orientation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-RESP=$(gdbus call --system --dest net.hadess.SensorProxy --object-path /net/hadess/SensorProxy --method org.freedesktop.DBus.Properties.Get net.hadess.SensorProxy AccelerometerOrientation)
-echo ${RESP:3:-4}
+RESP="$(gdbus call --system --dest net.hadess.SensorProxy --object-path /net/hadess/SensorProxy --method org.freedesktop.DBus.Properties.Get net.hadess.SensorProxy AccelerometerOrientation)"
+ORIENT="${RESP:3:-4}"
+
+if [ "${ORIENT}" = "" ]; then
+        echo normal
+elif [ "${ORIENT}" = "undefined" ]; then
+        echo normal
+else
+        echo "${ORIENT}"
+fi
