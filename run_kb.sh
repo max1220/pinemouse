fe95dc8931d2f12680d829f9f8c4a8b9d62302a1
Max <max@max1220.de>
2022-01-28 00:28:48 +0100

    fixed typo

--- run_kb.sh
+++ run_kb.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${0}")"
 TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
 KEYBOARD="/dev/input/by-path/platform-1c2b400.i2c-event-kbd"
 
-sudo ./touchpad_kb.lua "${TOUCHSCREEN}" "${KEYBOARD}" right-up
+sudo ./touchpad_keyboard.lua "${TOUCHSCREEN}" "${KEYBOARD}" right-up
