70525ffb6a71cd2879fb112d7d41963bc50cc80f
Max <max@max1220.de>
2022-01-28 00:15:01 +0100

    Added simple script for PinePhone keyboard as mouse buttons, with esc to stop

---
+++ run_kb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "$(dirname "${0}")"
+
+TOUCHSCREEN="/dev/input/by-path/platform-1c2ac00.i2c-event"
+KEYBOARD="/dev/input/by-path/platform-1c2b400.i2c-event-kbd"
+
+sudo ./touchpad_kb.lua "${TOUCHSCREEN}" "${KEYBOARD}" right-up
